836e1e5e0ad1cb89fe71780e2d56f920612cf493
Jorawer Grewal <joegrewal20@gmail.com>
2021-11-24 16:11:15 -0800

    Implement fetchMyIP and fetchCoordsByIp functions

---
+++ index.js
@@ -0,0 +1,18 @@
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+// fetchCoordsByIP('70.70.248.100', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned coordinates:', coordinates);
+// });
