1710a72aa3cded3ae98a40f30bcf8a3676e1a1ae
Jorawer Grewal <joegrewal20@gmail.com>
2021-11-24 22:53:34 -0800

    Remove test code

--- index.js
+++ index.js
@@ -1,31 +1,5 @@
 const { nextISSTimesForMyLocation} = require('./iss');
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-
-//   console.log('It worked! Returned IP:' , ip);
-// });
-
-// fetchCoordsByIP('70.70.248.100', (error, coordinates) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-//   console.log('It worked! Returned coordinates:', coordinates);
-// });
-
-// const myCoord = {latitude: 49.1049, longitude: -122.824};
-// fetchISSFlyOverTimes(myCoord, (error, flyOverTimes) => {
-//   if (error) {
-//     console.log("It didn't work!:" , error)
-//     return;
-//   }
-//   console.log('It worked! Returned fly over times:', flyOverTimes)
-// });
-
 const printPassTimes = (passTimes) => {
   passTimes.forEach( pass => {
     const dateTime = new Date(0);
@@ -39,6 +13,5 @@ nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
   }
-  // success, print out the deets!
   printPassTimes(passTimes);
 });
