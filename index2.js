50ef67a4d41d939aadb8b57a27151bc81596b6d3
Jorawer Grewal <joegrewal20@gmail.com>
2021-11-25 00:29:44 -0800

    Implement nextISSTimesForMyLocation function

--- index2.js
+++ index2.js
@@ -1,6 +1,15 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
 
-fetchMyIP()
-  .then(fetchCoordsByIP)
-  .then(fetchISSFlyOverTimes)
-  .then(body => console.log(body));
+const printPassTimes = (passTimes) => {
+  passTimes.forEach( pass => {
+    const dateTime = new Date(0);
+    dateTime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${dateTime} for ${duration} seconds!`);
+  }) 
+};
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
